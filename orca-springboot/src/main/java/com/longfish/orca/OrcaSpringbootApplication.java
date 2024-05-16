2255231ece5d1678be7dade56225f239146c797e
longfish <longfishes@qq.com>
2024-05-16 10:57:09 +0800

    initial springboot

---
+++ orca-springboot/src/main/java/com/longfish/orca/OrcaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.longfish.orca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcaSpringbootApplication.class, args);
+	}
+
+}
---
+++ orca-springboot/src/test/java/com/longfish/orca/OrcaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longfish.orca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
