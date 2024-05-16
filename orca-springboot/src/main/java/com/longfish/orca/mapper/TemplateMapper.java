b242e6e65e09344a157153d71f0dd00dd1da982d
longfish <longfishes@qq.com>
2024-05-16 12:04:34 +0800

    initial springboot

---
+++ orca-springboot/src/main/java/com/longfish/orca/mapper/TemplateMapper.java
@@ -0,0 +1,16 @@
+package com.longfish.orca.mapper;
+
+import com.longfish.orca.pojo.entity.Template;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author longfish
+ * @since 2024-05-16
+ */
+public interface TemplateMapper extends BaseMapper<Template> {
+
+}
---
+++ orca-springboot/src/main/java/com/longfish/orca/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.longfish.orca.mapper;
+
+import com.longfish.orca.pojo.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author longfish
+ * @since 2024-05-16
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
