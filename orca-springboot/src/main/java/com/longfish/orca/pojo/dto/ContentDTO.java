f04fd153f93b5a89e4985a593c6bb5a058f795a0
longfish <longfishes@qq.com>
2024-06-20 12:48:02 +0800

    fix bug of no constructor

--- orca-springboot/src/main/java/com/longfish/orca/pojo/dto/ContentDTO.java
+++ orca-springboot/src/main/java/com/longfish/orca/pojo/dto/ContentDTO.java
@@ -1,11 +1,15 @@
 package com.longfish.orca.pojo.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContentDTO {
 
     @Schema(requiredMode = Schema.RequiredMode.REQUIRED, example = "tt_content")
